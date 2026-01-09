c97f4ff18c519d2c7b6e39c9f1457ccca5c03487
Sarim Abdullah <abdullahsarim1100@gmail.com>
2026-01-09 18:40:03 +0530

    rectify

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -52,8 +52,8 @@ export async function signup(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "development" ? "lax" : "none", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "!development",
       path: "/",
     });
 
